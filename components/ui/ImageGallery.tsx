2c1e0d1644087121b15a8792948f2ba7326e2a95
abreel <64946264+abreel@users.noreply.github.com>
2025-08-03 10:08:43 +0000

    code update

--- components/ui/ImageGallery.tsx
+++ components/ui/ImageGallery.tsx
@@ -13,7 +13,9 @@ export const ImageGallery = ({ screenshots }: { screenshots: string[] }) => {
     screenshots?.length > 0 && (
       <section className="mb-10">
         <h2 className="text-2xl font-bold mb-4">Screenshots</h2>
-        <Gallery items={galleryItems} />
+        <div className="overflow-x-auto whitespace-nowrap scrollbar-hidden">
+          <Gallery items={galleryItems} />
+        </div>
       </section>
     )
   );

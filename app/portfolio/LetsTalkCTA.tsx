a97b44a4f8b5d0dde5ced779e6eac2d213069ba8
abreel <64946264+abreel@users.noreply.github.com>
2025-08-03 12:38:07 +0000

    updated let's talk CTA

--- app/portfolio/LetsTalkCTA.tsx
+++ app/portfolio/LetsTalkCTA.tsx
@@ -3,7 +3,7 @@ import { Button } from '@/components/ui/button';
 import Link from 'next/link';
 
 export const LetsTalkCTA = () => (
-  <div className="py-4 text-center space-y-4">
+  <div className="py-4 text-center space-y-4 px-4">
     <p>Need a project? Whether it&apos;s a modern web app, intuitive UI, or AI-powered product</p>
     <Button asChild size="lg" className="rounded-full px-6 py-4">
       <Link href="#contact">Let&apos;s Talk</Link>

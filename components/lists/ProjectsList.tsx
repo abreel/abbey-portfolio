9f296de11ff10749cb168bf232e3a6423afbc51b
abreel <64946264+abreel@users.noreply.github.com>
2025-08-03 12:40:16 +0000

    code update

--- components/lists/ProjectsList.tsx
+++ components/lists/ProjectsList.tsx
@@ -7,8 +7,6 @@ import { Card, CardContent } from '../ui/card';
 import { AnimatedImage } from '../ui/AnimatedImage';
 
 export default function ProjectsList({ projects }: { projects: Project[] }) {
-  console.log(projects);
-
   return (
     <motion.div
       className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 px-4"
